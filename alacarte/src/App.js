47d9c8db5f4d62a25514fda1340d40c7c0554475
Benadjaoud Abdelhak <89075329+hakouben@users.noreply.github.com>
2022-03-13 02:32:36 +0100

    Update App.js

--- alacarte/src/App.js
+++ alacarte/src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
 function App() {
-  return 
+  return
 }
 
 export default App;
