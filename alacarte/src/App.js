d96d4164d9b407419197f699329e87749c0f1b1e
Benadjaoud Abdelhak <89075329+hakouben@users.noreply.github.com>
2022-03-13 02:31:16 +0100

    starter

---
+++ alacarte/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { CssBaseline , Grid } from "@material-ui/core";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+function App() {
+  return 
+}
+
+export default App;
+
